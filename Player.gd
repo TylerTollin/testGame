cb83bf7ed6a32ea4fb3a99b458a3863013f4549c
Tyler <34544969+Exulansis0311@users.noreply.github.com>
2019-03-23 16:13:01 -0500

    Initial

---
+++ Player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody
+
+var speed = 350
+var direction = Vector3()
+var gravity = -9.8
+var velocity = Vector3()
+var hitCount = get_slide_count()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	direction = Vector3(0, 0, 0)
+	if Input.is_action_pressed("ui_left"):
+		direction.z -= 1 # direction = direction -1
+	
+	if Input.is_action_pressed("ui_right"):
+		direction.z += 1 # direction = direction + 1
+		
+	if Input.is_action_pressed("ui_up"):
+		direction.x += 1
+	
+	if Input.is_action_pressed("ui_down"):
+		direction.x -= 1
+		
+	direction = direction.normalized()
+	direction = direction * speed * delta
+	
+	velocity.y += gravity * delta
+	velocity.x = direction.x
+	velocity.z = direction.z
+	
+	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+	
+	if is_on_floor() and Input.is_key_pressed(KEY_SPACE):
+		velocity.y = 5
+		
+	if hitCount > 0:
+			var collision = get_slide_collision(0)
+			if collision.collider is RigidBody:
+				collision.collider.apply_impulse(collision.position, -collision.normal)
